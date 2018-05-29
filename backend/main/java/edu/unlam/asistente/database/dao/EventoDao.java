7f9df5655831da3d4075d4d64c62ead4b5b56d06
Ezequiel Laurenti <ezee.laurenti@gmail.com>
2018-05-28 23:54:40 -0300

    se añade logica de create de evento y test

--- backend/main/java/edu/unlam/asistente/database/dao/EventoDao.java
+++ backend/main/java/edu/unlam/asistente/database/dao/EventoDao.java
@@ -10,28 +10,26 @@ import edu.unlam.asistente.database.pojo.Evento;
 import edu.unlam.asistente.database.pojo.Usuario;
 import edu.unlam.asistente.database.dao.BaseDao;
 
-public class EventoDao extends BaseDao{
+public class EventoDao extends BaseDao {
 
 	public EventoDao() throws SQLException {
 		super();
 	}
-	
-	public List<Evento> obtenerEventosPorUsuario(Usuario user){
-		
+
+	public List<Evento> obtenerEventosPorUsuario(Usuario user) {
+
 		List<Evento> listaEventos = null;
 		Session session = null;
 		try {
 			session = factory.openSession();
-			String hql = "select distinct e from Evento e " +
-	                	"join e.usuarios u " +
-	                	"where u.usuario = :user";
+			String hql = "select distinct e from Evento e " + "join e.usuarios u " + "where u.usuario = :user";
 			Query query = session.createQuery(hql).setParameter("user", user.getUsuario());
-			
+
 			listaEventos = query.list();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			if (session != null) {
 				try {
 					session.close();
@@ -40,6 +38,39 @@ public class EventoDao extends BaseDao{
 				}
 			}
 			return listaEventos;
+		}
+	}
+	
+	public Evento obtenerEventoPorId(Integer id) {
+		
+		Evento evento = null;
+		Session session = null;
+		try {
+			session = factory.openSession();
+			evento = session.get(Evento.class, id);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null) {
+				session.close();;
+			}
+			return evento;
+		}
+	}
+
+	public void createEvent(Evento evento) {
+
+		Session session = null;
+		try {
+			session = factory.openSession();
+			session.saveOrUpdate(evento);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
 		}
 	}
 
--- backend/tests/edu/unlam/asistente/database/dao/EventoDaoTest.java
+++ backend/tests/edu/unlam/asistente/database/dao/EventoDaoTest.java
@@ -3,6 +3,8 @@ package edu.unlam.asistente.database.dao;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import org.junit.Before;
@@ -18,13 +20,14 @@ import org.junit.Assert;
 public class EventoDaoTest {
 	
 	List<Evento> listaEventos;
+	Evento nuevoEvento;
 	Usuario user;
-	EventoDao eventoUsuarioDao;
+	EventoDao eventoDao;
 	
 	@Before
 	public void setUp() throws SQLException, ParseException {
 		
-		eventoUsuarioDao = new EventoDao();
+		eventoDao = new EventoDao();
 		
 		user = new Usuario();
 		user.setId(1);
@@ -33,11 +36,27 @@ public class EventoDaoTest {
 		listaEventos = new ArrayList<>();
 		listaEventos.add(new Evento(1, "2018-05-22 01:10:08", "test event 1"));
 		listaEventos.add(new Evento(2, "2018-12-30 05:00:00", "test event 2"));
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(2);
+		usuario.setUsuario("usuarioTestInsert");
+		nuevoEvento = new Evento();
+		nuevoEvento.setFecha("2018-05-30 05:00:00");
+		nuevoEvento.setDescripcion("test insert");
+		nuevoEvento.setUsuarios(new HashSet<Usuario>(Arrays.asList(usuario)));
 	}
 	
 	@Test
 	public void obtenerEventosPorUsuarioTest() {
-		List<Evento> resultado = eventoUsuarioDao.obtenerEventosPorUsuario(user);
+		List<Evento> resultado = eventoDao.obtenerEventosPorUsuario(user);
 		Assert.assertTrue(listaEventos.equals(resultado));
+	}
+	
+	@Test
+	public void crearEventoTest() {
+		eventoDao.createEvent(nuevoEvento);
+		Evento aux = eventoDao.obtenerEventoPorId(nuevoEvento.getId());
+		Assert.assertEquals(nuevoEvento, aux);
+		
 	}
 }
